da0e07109319b102cde49bcb4c78498d3cc0b91b
Emilio Platzer <emilioplatzer@gmail.com>
2026-09-09 17:57:16 -0300

    Manda la pantalla de prueba de concepto.

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -6,6 +6,25 @@ app.get('/menu', (_, res) => {
     res.send('SSOT2026 - Solo Somos Otros Tenaces en 2026');
 })
 
+app.get('/poc/inter', (_, res) => {
+    res.send(`
+        <form method=post action="/poc/api/inter">
+            <p>Esta es una prueba de concepto</p>
+            <p><label>dato:<input name=dato></label></p>
+            <input type=submit value="Procesar">
+        </form>
+    `)
+})
+
+app.post('/poc/api/inter', async (req, res) => {
+    console.log('POST', '/poc/api/inter')
+    console.log(req.query);
+    res.send(`
+        <H2>recibido</H2>
+    `)
+    console.log(await req.body);
+})
+
 const port = 3000;
 
 app.listen(port, (error) => {
